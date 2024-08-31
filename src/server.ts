f417023cf748705034a0040b2be3e546d457ebb3
Parteeksachdeva <parteeksachdeva900@gmail.com>
2024-08-31 18:39:09 +0530

    feat: Initialize redis connection.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app";
-import { prisma } from "./data-source";
+import redisClient from "./redisConfig";
 
 const PORT = process.env.PORT || 3000;
+
+redisClient.connect().then(() => {
+  console.log("Redis connected");
+});
 
 app.listen(PORT, () => {
   console.log("Server listening on", PORT);
